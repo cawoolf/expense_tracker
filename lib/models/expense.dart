d7f03bd84c745cbfe7696c7b8b391a8fd1c840d2
cawoolf <destellodev@gmail.com>
2025-03-09 16:41:33 -0600

    131

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -17,7 +17,11 @@ const categoryIcons = {
 };
 
 class Expense {
-  Expense({required this.title, required this.amount, required this.date, required this.category})
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid.v4();
   final String id;
   final String title;
@@ -31,5 +35,27 @@ class Expense {
 
   Type get categories {
     return Category;
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();// Alternative constructor function
+  
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
