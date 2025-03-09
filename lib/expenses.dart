e2b22b2fbac25aba6e04f5b8a7df9abf4351e592
cawoolf <destellodev@gmail.com>
2025-03-08 18:29:43 -0700

    126

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -2,7 +2,7 @@ import 'package:expense_tracker/common_widgets/new_expense.dart';
 import 'package:expense_tracker/common_widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
-import '../models/expense.dart';
+import 'models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -38,28 +38,67 @@ class _ExpensesState extends State<Expenses> {
         category: Category.work),
   ];
 
+  // final List<Expense> _registeredExpenses = [];
+
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-      isScrollControlled: true, // Causes the overlay to take up the full screen
+        isScrollControlled: true,
+        // Causes the overlay to take up the full screen
         context: context,
-        builder: (ctx) => NewExpense(onAddExpense: _addExpense)); //ctx is the context related to the builder widget
+        builder: (ctx) => NewExpense(
+            onAddExpense:
+                _addExpense)); //ctx is the context related to the builder widget
   }
 
   void _addExpense(Expense expense) {
     setState(() {
-    _registeredExpenses.add(expense);
+      _registeredExpenses.add(expense);
     });
+
   }
 
   void _removeExpense(Expense expense) {
+
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    // ScaffoldMessenger shows overlays at the root Scaffold()
+    _triggerSnackBar(expenseIndex, expense);
+
+  }
+
+  void _triggerSnackBar(int expenseIndex, Expense expense) {
+    // ScaffoldMessenger shows overlays at the root Scaffold()
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Expense deleted.'),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          }),));
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    Widget mainContent = Center(
+      child: Text('No expenses found. Start adding some!'), // Default Content
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Flutter Expense Tracker'),
@@ -69,13 +108,7 @@ class _ExpensesState extends State<Expenses> {
           ],
         ),
         body: Column(
-          children: [
-            Text('The Chart'),
-            Expanded(
-                child: ExpensesList(
-              expenses: _registeredExpenses, onRemoveExpense: _removeExpense,
-            ))
-          ],
+          children: [Text('The Chart'), Expanded(child: mainContent)],
         ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,53 @@
-import 'package:expense_tracker/common_widgets/expenses.dart';
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
-  runApp(
-      const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: _appBarTheme(),
+        cardTheme: _cardTheme(),
+        elevatedButtonTheme: _elevatedButtonThemeData(),
+    textTheme: _textTheme()),
+    home: const Expenses(),
   ));
 }
 
+TextTheme _textTheme() {
+  return ThemeData().textTheme.copyWith(
+    titleLarge: TextStyle(fontWeight: FontWeight.normal, color: kColorScheme.onSecondaryContainer, fontSize: 14) // Title large is usually the title for appBars and other widgets
+  );
+}
+
+AppBarTheme _appBarTheme() {
+  return const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer); // Normally overrides any text color in the appBar
+}
+
+ElevatedButtonThemeData _elevatedButtonThemeData() {
+  return ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer));
+}
+
+CardTheme _cardTheme() {
+  return const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8));
+}
+
+
+// Notes
+/* Using ThemeData().copyWith() preservers some of the Flutter default themes. Allows individual aspects to be configured.
+colorScheme: is applied to several different widgets. Easy way to theme the app
+
+Selective overrides of theme elements is the way to go.
+
+k before variable name means it's global
+
+use .styleFrom() as a .copyWith() technique for Buttons
+*/
