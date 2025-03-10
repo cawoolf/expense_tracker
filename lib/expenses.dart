95b434046f1e5a50ecadf2b4becc376ce0909b54
cawoolf <destellodev@gmail.com>
2025-03-09 20:45:30 -0600

    103

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/common_widgets/chart/chart.dart';
 import 'package:expense_tracker/common_widgets/new_expense.dart';
 import 'package:expense_tracker/common_widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
@@ -108,7 +109,7 @@ class _ExpensesState extends State<Expenses> {
           ],
         ),
         body: Column(
-          children: [Text('The Chart'), Expanded(child: mainContent)],
+          children: [Chart(expenses: _registeredExpenses), Expanded(child: mainContent)],
         ));
   }
 }
